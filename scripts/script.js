2b0500028218a6067061f7d20c007c955fae5ef6
josefcarlsson <josef.carlsson@me.com>
2021-09-30 09:07:58 +0200

    SVG-Morpheus

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,24 @@
-function toggleState() {
-  document.querySelector(".toggle-me").classList.toggle("active");
-};
+var options = {
+  easing: "linear",
+  rotation: "none",
+}
+var currentIcon = 0;
+var arrayOfIcons = ["close", "square", "hamburger", "triangle", "circle", "sun"];
+var myIcons = new SVGMorpheus("#myIconSet", options);
+
+
+function startIconChange() {
+
+  setInterval(changeIcon, 1000);
+}
+
+function changeIcon() {
+
+  currentIcon = (currentIcon + 1 >= arrayOfIcons.length) ? 0 : currentIcon + 1;
+  myIcons.to(arrayOfIcons[currentIcon]);
+  if (arrayOfIcons[currentIcon] === "sun") {
+    document.getElementById("myIconSet").classList.add('sun');
+  } else {
+    document.getElementById("myIconSet").classList.remove("sun");
+  }
+}
