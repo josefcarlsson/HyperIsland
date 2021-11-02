0e8e56a32c0e6c6b575c1857955bf2bd357ff1de
josefcarlsson <josef.carlsson@me.com>
2021-11-02 15:48:36 +0100

    keybord working

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -1,8 +1,11 @@
-let activeStudent = null;
+let tempImage =
+ "https://images.aftonbladet-cdn.se/v2/images/e4f188ac-2fc3-4770-a651-7a10fa8d7c5f?fit=crop&format=auto&h=2052&q=50&w=1900&s=e05602163cc1737ec1153fff7c922be153ee50fd";
+
+let students = [];
 
 async function getStudents() {
  let res = await fetch('../assets/students.json')
- let students = await res.json();
+ students = await res.json();
  generateKeyboard(students);
 }
 
@@ -10,7 +13,11 @@ function generateKeyboard(students) {
  let keyboard = document.querySelector(".keyboard")
  for (let i = 0; i < students.length; i++) {
   keyboard.innerHTML += `
-   <button class="keyboard__key keyboard">${students[i].key}</button>
+   <button style="background-image:url(${
+     students[i].img ? students[i].img : tempImage
+   })" id="key-${students[i].key}" class="keyboard__key keyboard">${
+    students[i].key
+  }</button>
    `;
  }
 
@@ -19,4 +26,38 @@ function generateKeyboard(students) {
  `;
 }
 
+function findStudentFormKey(key) {
+ return students.filter(student => student.key === key);
+}
+
+function diplayStudent(student) {
+  console.log("student", student);
+ document.getElementById("active-student-information").innerHTML = `
+  <div>
+   ${student[0].name}
+  </div>
+ `;
+ 
+ let prev = document.querySelector(".keyboard__key--pressed")
+
+ if (prev) {
+  prev.classList.remove("keyboard__key--pressed");
+ }
+
+ document.getElementById(`key-${student[0].key}`).classList.add('keyboard__key--pressed');
+}
+
+function addEventListnerOnkeyboard() {
+ window.addEventListener("keydown", function (event) {
+   if (event.key !== undefined) {
+    let activeStudent = findStudentFormKey(event.key);
+    diplayStudent(activeStudent);
+     // Handle the event with KeyboardEvent.key
+   } else if (event.which !== undefined) {
+     // Handle the event with KeyboardEvent.which
+   }
+ });
+}
+
+addEventListnerOnkeyboard();
 getStudents();
